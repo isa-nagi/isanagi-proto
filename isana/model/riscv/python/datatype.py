92e67043065cf0635b23a7114f26dbe9f629c41b
nakandev <nakandev.s@gmail.com>
2025-07-11 10:40:19 +0900

    fix rixcv model datatype

--- isana/model/riscv/python/datatype.py
+++ isana/model/riscv/python/datatype.py
@@ -1,24 +1,34 @@
-from isana.isa import Immediate, ImmS, ImmU
+from isana.isa import ImmS, ImmU
+from .defs import xlen
 
-Imm = Immediate("Imm", width=32)
-ImmS12 = ImmS("ImmS12", width=12)
-ImmS12O1 = ImmS("ImmS12O1", width=12, offset=1)
-ImmS20O1 = ImmS("ImmS20O1", width=20, offset=1)
-ImmS20O12 = ImmS("ImmS20O12", width=20, offset=12)
-ImmS6 = ImmS("ImmS6", width=6)
-ImmS9 = ImmS("ImmS9", width=9)
-ImmS32O2 = ImmS("ImmS32O2", width=32, offset=2)
-ImmS5O2 = ImmS("ImmS5O2", width=5, offset=2)
-ImmS5O3 = ImmS("ImmS5O3", width=5, offset=3)
-ImmS5O4 = ImmS("ImmS5O4", width=5, offset=4)
-ImmS11O1 = ImmS("ImmS11O1", width=11, offset=1)
-ImmS6O2 = ImmS("ImmS6O2", width=6, offset=2)
-ImmS6O3 = ImmS("ImmS6O3", width=6, offset=3)
-ImmS6O4 = ImmS("ImmS6O4", width=6, offset=4)
-ImmS8O1 = ImmS("ImmS8O1", width=8, offset=1)
-ImmS2O4 = ImmS("ImmS2O4", width=2, offset=4)
-ImmU4 = ImmU("ImmU4", width=4)
-RMImm = ImmU("RMImm", width=3, enums={
+
+class ImmSXlen(ImmS):
+    base = f"i{xlen}"
+
+
+class ImmUXlen(ImmU):
+    base = f"u{xlen}"
+
+
+Imm = ImmSXlen("Imm", width=32)
+ImmS12 = ImmSXlen("ImmS12", width=12)
+ImmS12O1 = ImmSXlen("ImmS12O1", width=12, offset=1)
+ImmS20O1 = ImmSXlen("ImmS20O1", width=20, offset=1)
+ImmS20O12 = ImmSXlen("ImmS20O12", width=20, offset=12)
+ImmS6 = ImmSXlen("ImmS6", width=6)
+ImmS9 = ImmSXlen("ImmS9", width=9)
+ImmS32O2 = ImmSXlen("ImmS32O2", width=32, offset=2)
+ImmS5O2 = ImmSXlen("ImmS5O2", width=5, offset=2)
+ImmS5O3 = ImmSXlen("ImmS5O3", width=5, offset=3)
+ImmS5O4 = ImmSXlen("ImmS5O4", width=5, offset=4)
+ImmS11O1 = ImmSXlen("ImmS11O1", width=11, offset=1)
+ImmS6O2 = ImmSXlen("ImmS6O2", width=6, offset=2)
+ImmS6O3 = ImmSXlen("ImmS6O3", width=6, offset=3)
+ImmS6O4 = ImmSXlen("ImmS6O4", width=6, offset=4)
+ImmS8O1 = ImmSXlen("ImmS8O1", width=8, offset=1)
+ImmS2O4 = ImmSXlen("ImmS2O4", width=2, offset=4)
+ImmU4 = ImmUXlen("ImmU4", width=4)
+RMImm = ImmUXlen("RMImm", width=3, enums={
     0: "rne",
     1: "rtz",
     2: "rdn",
